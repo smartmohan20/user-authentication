4365160b8cc1479492d76b30c6d1c0daffa92a9d
smartmohan20 <dev.smartmohan@gmail.com~>
2024-04-23 11:19:25 +0530

    Installed cors and initialized

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Create the Express app
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 export default app;
