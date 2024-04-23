db82b3485a614a7592b46e3b87194a7d358c853b
smartmohan20 <dev.smartmohan@gmail.com~>
2024-04-23 11:29:31 +0530

    Used body parser middleware for application

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -12,6 +13,9 @@ const app = express();
 
 // Enable CORS
 app.use(cors());
+
+// Use body-parser middleware
+app.use(bodyParser.json());
 
 // Use the routes
 app.use('/', rootRoutes);
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,5 @@ const PORT = process.env.SERVER_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Server URL: http://localhost:${PORT}`);
 });
