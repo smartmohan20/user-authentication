0d839722951335eaf7d905be01de2627763abba9
smartmohan20 <dev.smartmohan@gmail.com~>
2024-04-23 11:25:27 +0530

    Implemented root testing endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,15 @@ import cors from 'cors';
 // Load environment variables from .env file
 dotenv.config();
 
+import rootRoutes from './routes/rootRoutes';
+
 // Create the Express app
 const app = express();
 
 // Enable CORS
 app.use(cors());
+
+// Use the routes
+app.use('/', rootRoutes);
 
 export default app;
